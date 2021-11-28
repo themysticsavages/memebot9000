3640d6d0766b84cc00360da13f560caa2d07893c
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-11-28 13:40:06 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,27 +1,24 @@
 from moviepy.video.VideoClip import ImageClip
 from moviepy.video.fx.resize import resize
 from moviepy.editor import VideoFileClip, AudioFileClip, CompositeAudioClip, concatenate
+from lib import Upload
 from PIL import Image
-from requests import api
-from lib import OCR
 import Create
-import gtts
-import json
 import os
 
 '''
 Final steps
 '''
 
-OCR_SPACE_KEY = json.loads(open('secrets/ocr_space_secrets.json').read())['key']
-def compile_memes_raw() -> list:
+def compile_memes_raw() -> None:
     clips = []
-    text = []
 
-    # for filename in os.listdir('images'):
-    #     os.remove(os.path.join('images', filename))
-    # Create.download_memes('images')
+    print('Cleaning previously used memes...')
+    for filename in os.listdir('images'):
+        os.remove(os.path.join('images', filename))
+    Create.download_memes('images')
 
+    print('Compressing images and attaching to video...')
     for filename in os.listdir('images'):
         if not filename.endswith('.mp4'):
             f = Image.open(os.path.join('images', filename))
@@ -33,30 +30,32 @@ def compile_memes_raw() -> list:
             except OSError:
                 pass
             
-            text.append(OCR.ocr_space_file(os.path.join('images', filename), overlay=True, api_key=OCR_SPACE_KEY))
             clip = ImageClip(os.path.join('images', filename))
             clip.duration = 4
             clip.fps = 10
 
             clips.append(clip)
 
+    print('Writing to memes_noaudio.mp4...')
     video = concatenate(clips, method='compose')
     video.write_videofile('memes_noaudio.mp4')
-    return text
+    return None
 def compile_memes(m) -> None:
-    gt = compile_memes_raw()
-    print(gt)
+    try:
+        os.remove('memes_noaudio.mp4')
+        os.remove('memes.mp4')
+    except FileNotFoundError:
+        pass
+
+    compile_memes_raw()
+
+    print('Adding default background music...')
     videoclip = VideoFileClip('memes_noaudio.mp4')
     audioclip = AudioFileClip(m)
-    i = 0
-
-    for text in gt:
-        tts = gtts.gTTS(text=text, lang='en', slow=True)
-        tts.save('assets/text{}.mp3'.format(i))
-        i += 1
 
     videoclip.audio = CompositeAudioClip([audioclip])
     videoclip.write_videofile('memes.mp4')
 
 if __name__ == '__main__':
     compile_memes('assets/default_music.mp3')
+    Upload.
